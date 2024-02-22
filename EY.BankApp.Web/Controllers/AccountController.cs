762823aa7934e7176220d3baea0322c96a998a89
emrecan.yilmaz <yilmazemrecu@gmail.com>
2024-02-22 13:36:34 +0300

    SendMoney operation and its views added

--- EY.BankApp.Web/Controllers/AccountController.cs
+++ EY.BankApp.Web/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using EY.BankApp.Web.Data.Repositories;
 using EY.BankApp.Web.Mapping;
 using EY.BankApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EY.BankApp.Web.Controllers
 {
@@ -51,7 +52,7 @@ namespace EY.BankApp.Web.Controllers
          public IActionResult GetByUserId(int userId)
         {
             var query = _accountRepository.GetQueryable();
-            var accounts=query.Where(x => x.Id == userId).ToList();
+            var accounts=query.Where(x => x.Id != userId).ToList();
             var user=_applicationUserRepository.GetById(userId);
             ViewBag.FullName = user.Name + " " + user.Surname;
             var list=new List<AccountListModel>();
@@ -74,6 +75,40 @@ namespace EY.BankApp.Web.Controllers
 
         }
 
+        public IActionResult SendMoney(int accountId)
+        {
+            var query=_accountRepository.GetQueryable();
+            var accounts = query.Where(x => x.Id != accountId).ToList();
+            
+            var list = new List<AccountListModel>();
+            ViewBag.SenderId = accountId;
+            foreach (var item in accounts)
+            {
+                list.Add(new()
+                {
+                    AccountNumber = item.AccountNumber,
+                    ApplicationUserId = item.ApplicationUserId,
+                    Balance = item.Balance,
+                    Id = item.Id
+                });
+            }
+            return View(new SelectList(list,"Id","AccountNumber"));
+        }
+
+        [HttpPost]
+        public IActionResult SendMoney(SendMoneyModel model)
+        {
+            var senderAccount=_accountRepository.GetById(model.SenderId);
+
+            senderAccount.Balance -= model.Amount;
+            _accountRepository.Update(senderAccount);
+
+            var account=_accountRepository.GetById(model.AccountId);
+            account.Balance += model.Amount;
+            _accountRepository.Update(account);
+            return RedirectToAction("Index", "Home");
+        }
+        
           
     }
 }
---
+++ EY.BankApp.Web/Models/SendMoneyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EY.BankApp.Web.Models
+{
+    public class SendMoneyModel
+    {
+        public int SenderId { get; set; }
+        public int AccountId { get; set; }
+        public int Amount { get; set; }
+
+    }
+}
